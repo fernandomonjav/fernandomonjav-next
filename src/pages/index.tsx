e9007508107876d6990d0c957542143ce604d3ba
Fernando Monja Vasquez <fernandomonjav@gmail.com>
2021-10-29 16:00:38 -0500

    Add latest projects to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
+import { GetStaticProps } from 'next'
 import { getLayout } from '~/components/common'
+import { ProjectCard } from '~/components/project'
 import { Container } from '~/components/ui'
+import { getProjects } from '~/lib/projects'
+import { Project } from '~/types'
 
-const HomePage = () => {
+interface Props {
+  projects: Project[]
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const projects = await getProjects({ limit: 3 })
+
+  return {
+    props: {
+      projects,
+    },
+  }
+}
+
+const HomePage = (props: Props) => {
+  const { projects } = props
+
   return (
     <Container>
       <div className="py-12">
@@ -11,6 +31,18 @@ const HomePage = () => {
           <p className="text-gray-600 text-lg md:text-xl">
             Developing technological tools that facilitate the activities performed by people.{' '}
           </p>
+        </div>
+      </div>
+
+      <div>
+        <h1 className="text-3xl font-bold">Latest projects</h1>
+      </div>
+
+      <div className="py-12">
+        <div className="grid gap-y-12 gap-x-8 md:grid-cols-2 lg:grid-cols-3">
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
         </div>
       </div>
     </Container>
